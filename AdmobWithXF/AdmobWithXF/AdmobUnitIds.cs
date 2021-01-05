4316d357501d14aa1a897a74591b3b23ea69b37c
omarheaba45@gmail.com <omarheaba45@gmail.com>
2021-01-06 01:57:46 +0200

    Add project files.

---
+++ AdmobWithXF/AdmobWithXF/AdmobUnitIds.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AdmobWithXF
+{
+    public class AdmobUnitIds
+    {
+        // For Banner Ad
+        private static string _bannerId { get; set; }
+        public static string BannerId
+        {
+            get
+            {
+                if (Device.RuntimePlatform == Device.Android)
+                    _bannerId = "ca-app-pub-3940256099942544/6300978111";
+                else if (Device.RuntimePlatform == Device.iOS)
+                    _bannerId = "ca-app-pub-3940256099942544/2934735716";
+
+                return _bannerId;
+            }
+        }
+
+        // For Interstitial Ad
+        private static string _interstitialId { get; set; }
+        public static string InterstitialId
+        {
+            get
+            {
+                if (Device.RuntimePlatform == Device.Android)
+                    _interstitialId = "ca-app-pub-3940256099942544/1033173712";
+                else if (Device.RuntimePlatform == Device.iOS)
+                    _interstitialId = "ca-app-pub-3940256099942544/4411468910";
+
+                return _interstitialId;
+            }
+        }
+
+        
+        // For Rewarded Ad
+        private static string _rewardedId { get; set; }
+        public static string RewardedId
+        {
+            get
+            {
+                if (Device.RuntimePlatform == Device.Android)
+                    _rewardedId = "ca-app-pub-3940256099942544/5224354917";
+                else if (Device.RuntimePlatform == Device.iOS)
+                    _rewardedId = "ca-app-pub-3940256099942544/1712485313";
+
+                return _rewardedId;
+            }
+        }
+    }
+}
---
+++ AdmobWithXF/AdmobWithXF/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using MarcTron.Plugin;
+
+namespace AdmobWithXF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.bannerAd_view.AdsId = AdmobUnitIds.BannerId;
+        }
+
+        private void loadInterstitialAd_Clicked(object sender, EventArgs e)
+        {
+            CrossMTAdmob.Current.LoadInterstitial(AdmobUnitIds.InterstitialId);
+        }
+
+        private void showInterstitialAd_Clicked(object sender, EventArgs e)
+        {
+            CrossMTAdmob.Current.ShowInterstitial();
+        }
+
+        private void loadRewardedAd_Clicked(object sender, EventArgs e)
+        {
+            CrossMTAdmob.Current.LoadRewardedVideo(AdmobUnitIds.RewardedId);
+        }
+
+        private void showRewardedAd_Clicked(object sender, EventArgs e)
+        {
+            CrossMTAdmob.Current.ShowRewardedVideo();
+        }
+    }
+}
